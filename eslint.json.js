e6ec2fbdb50828cd29d93ee036384b93eeb62bbe
ksdeveloper0201 <ksdeveloper0201@gmail.com>
2024-07-03 00:37:01 +0900

    format実行

---
+++ eslint.json.js
@@ -0,0 +1,20 @@
+const config = [
+  {
+    extends: [
+      "next/core-web-vitals",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:prettier/recommended",
+    ],
+    plugins: ["@typescript-eslint"],
+    rules: {
+      "@typescript-eslint/no-unused-vars": ["error"],
+      "prettier/prettier": ["error"],
+    },
+    parserOptions: {
+      project: "./tsconfig.json",
+      tsconfigRootDir: __dirname,
+      sourceType: "module",
+    },
+  },
+];
+export default config;
